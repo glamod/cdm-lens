f353b6bbc84a2072f4454c55f1597b3db7121345
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-01-05 17:14:30 +0000

    Updated data policies and citation code

--- cdm_interface/data_policies.py
+++ cdm_interface/data_policies.py
@@ -4,11 +4,11 @@ from io import StringIO
 from cdm_interface._loader import local_conn_str
 
 
-_data = """
+RUBBISH_data = """
 country_id|country|institute|data_policy_link|data_policy
 FI|Finland|Finnish Met Institute|https://en.ilmatieteenlaitos.fi/open-data|5
 GM|Germany|DWD|https://opendata.dwd.de/climate_environment/CDC/Terms_of_use.pdf|0
-EI|Ireland|Met ▒ireann|https://www.met.ie/climate/available-data/historical-data|5
+EI|Ireland|Met Éireann|https://www.met.ie/climate/available-data/historical-data|5
 LU|Luxembourg|Meteolux|https://www.meteolux.lu/fr/aide/aspects-legaux/?lang=fr, no additional data - https://community.wmo.int/notifications|5
 NL|Netherlands|KNMI|https://www.knmi.nl/copyright|5
 NO|Norway|MET Norway|https://www.met.no/en/free-meteorological-data/licensing-and-crediting|5
@@ -16,6 +16,8 @@ SW|Sweden|SMHI|https://www.smhi.se/omsmhi/policys/datapolicy/mer-och-mer-oppna-d
 US|United States|NOAA NCEI|https://www.ncdc.noaa.gov/wdcmet, https://obamawhitehouse.archives.gov/sites/default/files/omb/memoranda/2013/m-13-13.pdf|0
 """.strip()
 
+NATIONAL_POLICIES_FILE = '/usr/local/cdm_lens/tables/national_data_policies.psv' 
+SOURCE_CONFIG_FILE = '/usr/local/cdm_lens/tables/source_configuration.psv'
 
 ndp = None
 
@@ -23,8 +25,8 @@ ndp = None
 def get_national_data_policies():
     global ndp
 
-    if not ndp:
-        ndp = pd.read_csv(StringIO(_data), sep='|')
+    if ndp is None:
+        ndp = pd.read_csv(NATIONAL_POLICIES_FILE, sep='|')
 
     return ndp
 
@@ -45,15 +47,31 @@ def _get_policies_by_source(df):
     if not source_ids:
         return []
 
-    source_ids_string = ', '.join(source_ids)
-    source_sql = 'SELECT source_id, product_name, product_references, product_citation' \
-                 f' FROM source_configuration WHERE source_id IN ({source_ids_string});'
-    df = _select(source_sql)
-    return df
+#    source_sql = 'SELECT source_id, product_name, product_references, product_citation' \
+#                 f' FROM source_configuration WHERE source_id IN ({source_ids_string});'
+#    df = _select(source_sql)
+
+    source_config = pd.read_csv(SOURCE_CONFIG_FILE, sep='|')
+    source_df = source_config[source_config['source_id'].isin(source_ids)]
+
+    # Return filtered df containing only the required fields
+    req_fields = ['source_id', 'product_name', 'product_references', 'product_citation']
+    return source_df[req_fields]
+
+
+def _title(n):
+    return n.replace('_', ' ').title()
+
+
+def _value(v):
+    if str(v).lower() == 'nan':
+        return '-'
+    else:
+        return v
 
 
 def _get_policies_by_nation(df):
-    country_ids = set([_[:2] for _ in df['observation_id']unique()])
+    country_ids = set([_[:2] for _ in df['observation_id'].unique()])
     
     ndps = get_national_data_policies()
     countries_with_policies = set(ndps.country_id.unique())
@@ -62,7 +80,9 @@ def _get_policies_by_nation(df):
     countries = countries_with_policies & country_ids 
 
     # Extract and return records for countries with matching policies
-    return df[df['observation_id'].str.slice(0, 2).str.contains('|'.join(countries), regex=True)]
+    national_policies = ndps[ndps['country_id'].isin(list(countries))]
+#df[df['observation_id'].str.slice(0, 2).str.contains('|'.join(countries), regex=True)]
+    return national_policies
 
 
 def _render_data_policies(pols):
@@ -73,37 +93,47 @@ def _render_data_policies(pols):
 
     # Render by source first
     res += '## Data Policies derived from Sources\n\n'
+    by_source = ''
 
     for _, row in pols['by_source'].iterrows():
-        res += f'Source: {row["product_name"]} (ID: {row["source_id"]}):\n' 
+        by_source += f'Source: {row["product_name"]} (ID: {row["source_id"]}):\n' 
         
+        found = False
         for key in ('product_name', 'product_references', 'product_citation'):
             if key in row:
-                res += f'\t{key}: {row[key]}\n'
+                found = True
+                by_source += f'\t{_title(key)}: {_value(row[key])}\n'
 
-        else:
-            res += '\tNo additional information available.\n'
+        if not found:
+            by_source += '\tNo additional information available.\n'
 
-        res += '\n'
+        by_source += '\n'
 
+    if by_source:
+        res += by_source
     else:
         res += 'No data policy information derived from Source information.\n'
 
     # Now render by nation
     res += '\n## National Data Policies\n\n'
+    by_nation = ''
 
     for _, row in pols['by_nation'].iterrows():
-        res += f'National Data Policy Information: {row["country"]} (ID: {row["country_id"]}):\n'
+        by_nation += f'National Data Policy Information: {row["country"]} (ID: {row["country_id"]}):\n'
 
+        found = False
         for key in ('data_policy_link', 'institute'):
             if key in row:
-                res += f'\t{key}: {row[key]}\n'
+                found = True
+                by_nation += f'\t{_title(key)}: {_value(row[key])}\n'
 
-        else:
+        if not found:
             raise Exception(f'Error when retrieving National Data Policy info for: {row["country_id"]}')
 
-        res += '\n'
+        by_nation += '\n'
 
+    if by_nation:
+        res += by_nation
     else:
         res += 'No data policy information derived from national policies.\n'
 
@@ -132,6 +162,15 @@ def test_get_national_data_policies():
     _ = get_national_data_policies()
     assert(len(_) == 8)
     assert(_.loc[7].country == 'United States')
+
+    _data = """observation_id,data_policy_licence,date_time,date_time_meaning,observation_duration,longitude,latitude,report_type,height_above_surface,observed_variable,units,observation_value,value_significance,platform_type,station_type,primary_station_id,station_name,quality_flag,source_id
+NLM00006253-1-1999-03-02-00:00-85-12,WMO essential,1999-03-02 00:00:00+00:00,beginning,instantaneous,2.067,56.4,SYNOP,2.0,air temperature,K,279.35,Instantaneous value of observed parameter,,Land station,NLM00006253,AUK-ALFA,Passed,251
+NOM00006253-1-1999-03-02-00:00-85-12,WMO essential,1999-03-02 00:00:00+00:00,beginning,instantaneous,2.067,56.4,SYNOP,2.0,air temperature,K,279.35,Instantaneous value of observed parameter,,Land station,NLM00006253,AUK-ALFA,Passed,225
+EIM00006253-1-1999-03-02-00:00-85-12,WMO essential,1999-03-02 00:00:00+00:00,beginning,instantaneous,2.067,56.4,SYNOP,2.0,air temperature,K,279.35,Instantaneous value of observed parameter,,Land station,NLM00006253,AUK-ALFA,Passed,291"""
+
+    df = pd.read_csv(StringIO(_data), sep=',')
+    pols = get_data_policies(df)
+    print(pols)
 
 
 if __name__ == '__main__':
